bf0917248f2ef162f87219d276de90a2dc3d03db
rizky-wahyudi <rizky.wahyudi.id@email.com>
2020-10-23 01:56:31 +0700

    Penambahan fitur input Loker

--- loker.go
+++ loker.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"reflect"
 	"strconv"
 	"strings"
 )
@@ -44,6 +45,37 @@ func isLokerInitialized(loker []Loker) bool {
 	return false
 }
 
+func findEmptyLoker(loker []Loker) int {
+	for i := 0; i < len(loker); i++ {
+		if reflect.ValueOf(loker[i]).IsZero() {
+			return i
+		}
+	}
+	return -1
+}
+
+// Input Loker
+func inputLoker(command []string, loker *[]Loker) (bool, int, error) {
+	if len(command) == 3 {
+		emptyLoker := findEmptyLoker(*loker)
+		id, err := strconv.Atoi(command[2])
+		if err != nil {
+			return false, 0, err
+		}
+		if emptyLoker != -1 {
+			_loker := Loker{num: emptyLoker + 1, itemType: command[1], itemId: id}
+			(*loker)[emptyLoker] = _loker
+			return true, emptyLoker + 1, nil
+		} else {
+			return false, 0, errors.New("Locker Full")
+		}
+	} else if len(command) > 3 {
+		return false, 0, fmt.Errorf("Too much argument, expected 2, have %d", len(command)-1)
+	} else {
+		return false, 0, fmt.Errorf("Too few argument, expected 2, have %d", len(command)-1)
+	}
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	var loker []Loker
@@ -76,7 +108,16 @@ func main() {
 
 			// Input
 			case strings.EqualFold(commandSplit[0], "input"):
-				fmt.Println("input function")
+				if !isLokerInitialized(loker) {
+					fmt.Println("You haven't initialized the Locker yet")
+				} else {
+					res, info, err := inputLoker(commandSplit, &loker)
+					if res {
+						fmt.Println("Id Card stored in loker", info)
+					} else {
+						fmt.Println("Error:", err)
+					}
+				}
 				break
 
 			// Leave
