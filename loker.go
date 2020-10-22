f96ec922ab4d5019177a69e89363ed9375ffacc9
rizky-wahyudi <rizky.wahyudi.id@email.com>
2020-10-23 01:01:53 +0700

    Penambahan fitur init Loker

--- loker.go
+++ loker.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -13,12 +15,41 @@ type Loker struct {
 	itemId   int
 }
 
+// Initialize Loker
+func initializeLoker(command []string, loker *[]Loker) (bool, error) {
+	if len(command) == 2 {
+		size, err := strconv.Atoi(command[1])
+		if err != nil {
+			return false, err
+		} else {
+			if size >= 1 {
+				*loker = make([]Loker, size)
+				return true, nil
+			} else {
+				return false, errors.New("Cannot initialize loker less than 1")
+			}
+		}
+	} else if len(command) > 2 {
+		return false, fmt.Errorf("Too much argument, expected 1, have %d", len(command)-1)
+	} else {
+		return false, fmt.Errorf("Too few argument, expected 1, have %d", len(command)-1)
+	}
+}
+
+// Check if loker initialized or not
+func isLokerInitialized(loker []Loker) bool {
+	if len(loker) > 0 {
+		return true
+	}
+	return false
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+	var loker []Loker
 	var command string
 	var commandSplit []string
 	for {
-
 		scanner.Scan()
 		command = scanner.Text()
 
@@ -30,39 +61,44 @@ func main() {
 
 			//Init Loker
 			case strings.EqualFold(commandSplit[0], "init"):
-				fmt.Println("init function")
+				res, err := initializeLoker(commandSplit, &loker)
+				if res {
+					fmt.Println("Loker initialized with", len(loker), "slot(s)")
+				} else {
+					fmt.Println("Error:", err)
+				}
 				break
 
-				// Status
+			// Status
 			case strings.EqualFold(commandSplit[0], "status"):
 				fmt.Println("status function")
 				break
 
-				// Input
+			// Input
 			case strings.EqualFold(commandSplit[0], "input"):
 				fmt.Println("input function")
 				break
 
-				// Leave
+			// Leave
 			case strings.EqualFold(commandSplit[0], "leave"):
 				fmt.Println("leave function")
 				break
 
-				// Find
+			// Find
 			case strings.EqualFold(commandSplit[0], "find"):
 				fmt.Println("find function")
 				break
 
-				// Search
+			// Search
 			case strings.EqualFold(commandSplit[0], "search"):
 				fmt.Println("search function")
 				break
 
-				// Exit
+			// Exit
 			case strings.EqualFold(commandSplit[0], "exit"):
 				os.Exit(0)
 
-				// Default: No command found
+			// Default: No command found
 			default:
 				fmt.Println("No command found with name", commandSplit[0])
 			}
